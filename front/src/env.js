920eb0527b373301612b9e5e8b8b7e4deb182495
KlimPal <klimpal256@gmail.com>
2021-02-14 22:49:51 +0200

    Delete env.js

--- front/src/env.js
+++
@@ -1,5 +0,0 @@
-(function (window) {
-    window.__env = window.__env || {};
-    window.__env.httpApiBaseUrl = 'http://localhost:8080';
-    window.__env.wsApiBaseUrl = 'ws://localhost:8080';
-}(this));
