c4ab3b53a37ff8366d9191df80d62c2f49139365
KlimPal <klimpal256@gmail.com>
2021-03-12 15:27:21 +0200

    Add rxjs test

--- back/lib/use_cases/_index.js
+++ back/lib/use_cases/_index.js
@@ -2,6 +2,7 @@ import * as authentication from './authentication.js'
 import * as user from './user.js'
 import * as metrics from './metrics.js'
 import * as files from './files.js'
+import * as test from './test.js'
 
 export {
     authentication,
---
+++ back/lib/use_cases/test.js
@@ -0,0 +1,34 @@
+import * as rx from 'rxjs'
+import * as rxop from 'rxjs/operators/index.js'
+
+import cf from '../utils/cf.js'
+
+let testObservable
+
+//  testObservable = new rx.Observable(async (subscriber) => {
+//     while (true) {
+//         // eslint-disable-next-line no-await-in-loop
+//         await cf.sleep(Math.random() * 1000)
+//         subscriber.next('hello')
+//     }
+// })
+testObservable = rx.interval(100)
+
+let start = Date.now()
+
+
+let testObservable1 = testObservable
+    .pipe(
+        rxop.skipWhile((x) => x < 20),
+        rxop.sampleTime(300),
+        rxop.take(101),
+        rxop.bufferCount(5),
+    ).subscribe({
+        next(x) {
+            console.log(x)
+        },
+        complete() {
+        // console.log(Date.now() - start)
+        },
+    })
+
