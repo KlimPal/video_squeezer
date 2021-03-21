1c2dddd86133afc65ffb967ab6578043678614fb
KlimPal <klimpal256@gmail.com>
2021-03-21 18:15:38 +0200

    Update convert_video.js

--- workers/video_converter/lib/use_cases/convert_video.js
+++ workers/video_converter/lib/use_cases/convert_video.js
@@ -24,99 +24,82 @@ async function convertVideo({
     progressCallback,
 }) {
     const start = Date.now()
-
     const result = { bucket: targetBucket, key: targetKey }
 
 
-    const tmpSourceFileName = `${cf.generateUniqueCode(24)}_${sourceExtension}`
+    const tmpSourceFileName = `${cf.generateUniqueCode(24)}${sourceExtension}`
     const tmpSourcePath = path.join(config.tmpDirPath, tmpSourceFileName)
 
-    const downloadStartAt = Date.now()
-    await minioClient.fGetObject(sourceBucket, sourceKey, tmpSourcePath)
+    const downloadStartedAt = Date.now()
+    const filesToRemove = []
 
-    const fileStat = await fs.stat(tmpSourcePath)
-    logger.info({
-        jobId,
-        action: 'DOWNLOAD_SOURCE_FILE',
-        fileSize: fileStat.size,
-        fileSizeAsString: cf.getFriendlyFileSize(fileStat.size),
-        duration: Date.now() - downloadStartAt,
-    }, 'Job in progress')
-
-    // try {
-    //     const progressState = {
-    //         download: {
-    //             weight: 0.45,
-    //             fileMap: {},
-    //         },
-    //         convert: {
-    //             weight: 0.1,
-    //         },
-    //         upload: {
-    //             weight: 0.45,
-    //         },
-    //     }
-
-    //     await Promise.all(videoParts.map(async ({ sourceUrl, localPath }) => {
-    //         const response = await fetch(sourceUrl, { method: 'get' })
-    //         if (response.status !== 200) {
-    //             throw new Error('Unable to download video part')
-    //         }
-
-    //         let partSize = Number(response.headers.get('content-length'))
-    //         const progressWatcher = progressStream({
-    //             length: partSize,
-    //             time: 1000,
-    //         })
-
-    //         progressWatcher.on('progress', (progress) => {
-    //             progressState.download.fileMap[localPath] = progress.percentage
-    //             let values = Object.values(progressState.download.fileMap)
-    //             progressCallback((cf.sum(values) / videoParts.length) * progressState.download.weight)
-    //         })
-
-    //         await cf.pipeToFinish(response.body, progressWatcher, fs.createWriteStream(localPath))
-    //     }))
-
-    //     let fileListData = videoParts.map(({ localPath }) => `file '${localPath}'`).join('\n')
-
-    //     await fs.writeFile(fileListPath, fileListData)
-
-    //     const command = `ffmpeg -f concat -safe 0 -loglevel error -i ${fileListPath} -c copy ${outputPath}`
-    //     let { stderr } = await exec(command)
-    //     stderr = stderr.trim()
-    //     if (stderr) {
-    //         throw new Error(stderr)
-    //     }
-
-    //     if (!await fs.pathExists(outputPath)) {
-    //         throw new Error('Something went wrong. Output file not found.')
-    //     }
-    //     const downloadAndConvertProgress = 100 * (progressState.download.weight + progressState.convert.weight)
-    //     progressCallback(downloadAndConvertProgress)
-
-    //     let outputFileStat = await fs.stat(outputPath)
-
-    //     await minioClient.fPutObject(targetBucket, targetKey, outputPath)
-    //     progressCallback(100)
+    try {
+        filesToRemove.push(tmpSourcePath)
+        await minioClient.fGetObject(sourceBucket, sourceKey, tmpSourcePath)
 
 
-    //     result = {
-    //         duration: Date.now() - start,
-    //         outputFileSize: outputFileStat.size,
-    //     }
-    // } catch (e) {
-    //     await Promise.all(filesToRemove.map(path => fs.remove(path)))
+        const fileStat = await fs.stat(tmpSourcePath)
+        logger.info({
+            jobId,
+            action: 'DOWNLOAD_SOURCE_FILE',
+            fileSize: fileStat.size,
+            fileSizeAsString: cf.getFriendlyFileSize(fileStat.size),
+            duration: Date.now() - downloadStartedAt,
+        }, 'Job in progress')
 
-    //     console.log(e)
-    //     throw e
-    // } finally {
-    //     await Promise.all(filesToRemove.map(path => fs.remove(path)))
-    // }
 
-    // console.log(`\nEnd job #${prefix}`)
-    // console.log(`duration: ${result.duration} ms`)
-    // console.log(`outputFileSize: ${cf.getFriendlyFileSize(result.outputFileSize)}`)
+        const { crf, height, ffmpegPreset = 'veryfast' } = convertingOptions
+
+        const outputFilePath = path.join(config.tmpDirPath, `${cf.generateUniqueCode(24)}_converted.mp4`)
+
+        filesToRemove.push(outputFilePath)
+        const command = `ffmpeg -loglevel error -i "${tmpSourcePath}" `
+            + `-filter:v scale=-2:${height} -c:v libx264 -preset ${ffmpegPreset} `
+            + `-crf ${crf} "${outputFilePath}"`
+
+        const convertingStartedAt = Date.now()
+        let { stderr } = await exec(command)
+        stderr = stderr.trim()
+        if (stderr) {
+            throw new Error(stderr)
+        }
+
+        const outputFileStat = await fs.stat(outputFilePath)
+
+        logger.info({
+            jobId,
+            action: 'CONVERT',
+            ffmpegCommand: command,
+            outputFileSize: outputFileStat.size,
+            outputFileSizeAsString: cf.getFriendlyFileSize(outputFileStat.size),
+            duration: Date.now() - convertingStartedAt,
+        }, 'Job in progress')
+
+        result.fileSize = outputFileStat.size
+        result.fileSizeAsString = cf.getFriendlyFileSize(outputFileStat.size)
+
+        const uploadStartedAt = Date.now()
+        await minioClient.fPutObject(targetBucket, targetKey, outputFilePath)
+
+        logger.info({
+            jobId,
+            action: 'UPLOAD',
+            duration: Date.now() - uploadStartedAt,
+            outputFileSize: outputFileStat.size,
+            outputFileSizeAsString: cf.getFriendlyFileSize(outputFileStat.size),
+        }, 'Job completed')
+    } catch (error) {
+        await Promise.all(filesToRemove.map(path => fs.remove(path)))
+
+        logger.error({
+            error,
+        })
+        throw error
+    } finally {
+        await Promise.all(filesToRemove.map(path => fs.remove(path)))
+    }
+
+    result.jobDuration = Date.now() - start
 
     return result
 }
