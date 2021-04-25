7b222707c29e3438f8a42cf97a167725e5143093
KlimPal <klimpal256@gmail.com>
2021-04-25 16:34:35 +0300

    Create 20210425161523_minio_servers.js

---
+++ back/lib/services/knex/migrations/20210425161523_minio_servers.js
@@ -0,0 +1,28 @@
+export async function up(knex, Promise) {
+    await knex.schema.createTable('minio_servers', (table) => {
+        table.string('id', 16).primary()
+        table.string('host').notNullable()
+        table.integer('port').notNullable()
+        table.integer('status')
+        table.timestamp('created_at').defaultTo(knex.fn.now())
+        table.timestamp('updated_at').defaultTo(knex.fn.now())
+        table.timestamp('deleted_at')
+    })
+
+    await knex.schema.alterTable('files', (table) => {
+        table.string('minio_server_id', 16).references('id').inTable('minio_servers').onDelete('CASCADE')
+    })
+    await knex.schema.alterTable('file_parts', (table) => {
+        table.string('minio_server_id', 16).references('id').inTable('minio_servers').onDelete('CASCADE')
+    })
+}
+
+export async function down(knex, Promise) {
+    await knex.schema.alterTable('files', (table) => {
+        table.dropColumn('minio_server_id')
+    })
+    await knex.schema.alterTable('file_parts', (table) => {
+        table.dropColumn('minio_server_id')
+    })
+    await knex.schema.dropTable('minio_servers')
+}
